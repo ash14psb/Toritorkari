94b68b92729e4f61448970a301c1385dae86c221
Ashikur Rahman <86622343+ash14psb@users.noreply.github.com>
2024-05-31 11:08:00 +0600

    a

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Live Link
-
+([https://ash14psb.github.io/Toritorkari/](https://ash14psb.github.io/Toritorkari/)) 
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/toritorkari/', // Ensure this matches your GitHub Pages repository name
+  base: '/toritorkari/',
+  build: {
+    outDir: 'dist' 
+  }
 });
